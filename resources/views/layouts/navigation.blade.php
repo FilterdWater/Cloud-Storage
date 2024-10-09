854965fa7f2b3ca6802da2d2177e07b4669b59c4
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2024-10-09 08:43:38 +0200

    Created 3 views, Added them as Routes to web.php, Added them to the (Breeze) navigation menu
    Making this commit so i have a tutorial if i forget how to do this

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,21 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('my-files')" :active="request()->routeIs('my-files')">
+                        {{ __('My Files') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('shared')" :active="request()->routeIs('shared')">
+                        {{ __('Shared') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('shared-with-me')" :active="request()->routeIs('shared-with-me')">
+                        {{ __('Shared with me') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -69,6 +84,15 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('my-files')" :active="request()->routeIs('my-files')">
+                {{ __('My Files') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('shared')" :active="request()->routeIs('shared')">
+                {{ __('Shared') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('shared-with-me')" :active="request()->routeIs('shared-with-me')">
+                {{ __('Shared with me') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/my-files', function () {
+    return view('my-files');
+})->middleware(['auth', 'verified'])->name('my-files');
+
+Route::get('/shared', function () {
+    return view('shared');
+})->middleware(['auth', 'verified'])->name('shared');
+
+Route::get('/shared-with-me', function () {
+    return view('shared-with-me');
+})->middleware(['auth', 'verified'])->name('shared-with-me');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
