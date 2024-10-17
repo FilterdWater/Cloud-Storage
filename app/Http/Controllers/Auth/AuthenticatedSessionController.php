c694e48f2499a1c867ffa54aa2a1503f3e2adae8
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2024-10-17 11:58:05 +0200

    Send specific user to specific page after login based on role
    Wanted users with the Admin role to be sent to the dashboard page and users with the User role to be sent to the my-files page

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,16 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = Auth::user();
+
+        if ($user) {
+            if ($user->role_id === 1) {
+                return redirect()->intended(route('dashboard', absolute: false));
+            } elseif ($user->role_id === 2) {
+                return redirect()->intended(route('my-files', absolute: false));                
+            }
+        }
+
     }
 
     /**
