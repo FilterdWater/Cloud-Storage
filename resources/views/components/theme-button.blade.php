deae3e7e84efb743273fea7569cc4d2449adf815
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2024-10-19 13:09:58 +0200

    App wide code for theme switching added

--- resources/views/components/theme-button.blade.php
+++ resources/views/components/theme-button.blade.php
@@ -1,24 +1,3 @@
-<script>
-    (function() {
-        // Function to apply dark mode based on local storage or system preference
-        const applyTheme = () => {
-            const isDark = localStorage.theme === 'dark' ||
-                (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-            if (isDark) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-        };
-
-        applyTheme();
-
-        // Listen for changes in system color scheme preferences
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applyTheme);
-    })();
-</script>
-
 <div class="relative" x-data="{
     menu: false,
     theme: localStorage.theme,
