8755a5b4894eef8ffaffbe59b0895bd165842822
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2024-10-24 13:57:04 +0200

    Added default users for testing
    2 test users now get made so i dont have to do it manually anymore one with regular user priviliges and one with admin priviliges

--- database/migrations/2024_10_09_000002_add_role_id_to_users_table.php
+++ database/migrations/2024_10_09_000002_add_role_id_to_users_table.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,13 +14,36 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             // Add the role_id foreign key
-            $table->foreignId('role_id')
-            ->default(2)
-            ->after('id')
-            ->constrained('roles')
-            ->onUpdate('cascade') // If the role_id in the roles table is updated, it cascades to the users table
-            ->onDelete('restrict'); // Prevents deletion of the role if any user is assigned to it
+            $table
+                ->foreignId('role_id')
+                ->default(2)
+                ->after('id')
+                ->constrained('roles')
+                ->onUpdate('cascade') // If the role_id in the roles table is updated, it cascades to the users table
+                ->onDelete('restrict'); // Prevents deletion of the role if any user is assigned to it
         });
+
+        // Insert test users only if "APP_ENV=production" inside the .env file
+        if (!app()->environment('production')) {
+            DB::table('users')->insert([
+                [
+                    'name' => 'Admin User',
+                    'email' => 'admin@admin.com',
+                    'password' => Hash::make('zxcasdqwe'),
+                    'role_id' => 1, // Assuming 'Admin' role has an ID of 1
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'name' => 'Regular User',
+                    'email' => 'user@user.com',
+                    'password' => Hash::make('zxcasdqwe'),
+                    'role_id' => 2, // Assuming 'User' role has an ID of 2
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]);
+        }
     }
 
     /**
